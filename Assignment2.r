3ad898dae6b131255fc06dd841c88fca20a365f5
YuJieQiu0823712 <chiou.jade@gmail.com>
2023-05-06 17:42:29 +0200

    +Q3

--- Assignment2.r
+++ Assignment2.r
@@ -26,7 +26,7 @@ dim(data) # 97 8 ==> 8 variables for 97 data points
 sum(is.na(data)) #0
 data$svi<-as.factor(data$svi)
 str(data)
-
+View(data)
 summary(data)
 summarise(data, mean(Cscore), median(Cscore), n=n(), sd(Cscore))
 
@@ -92,9 +92,9 @@ omit_data <- data[-c(96),] # remove row when index 96
 sd(omit_data$Cscore) # => after omit, the standard deviation decrease 52->40
 var(omit_data$Cscore) #  => after omit, the variance decrease 2779 -> 1601
 
-data.lm.omit = lm(Cscore ~ ., data = omit_data)
-summary(data.lm.omit) #Residual standard error decreased
-
+data.lm.all.omit = lm(Cscore ~ ., data = omit_data)
+summary(data.lm.all.omit) #Residual standard error decreased
+#lcp,lpsa
 
 ## best Subset Selection 
 best <- regsubsets(Cscore~.,data=omit_data)
@@ -137,27 +137,78 @@ coef(bwd, 2)
 # 8.5  21.2 
 # => lcp,lpsa 
 
+
+data.lm.omit = lm(Cscore ~ lcp + lpsa, data = omit_data)
+summary(data.lm.omit) 
+#8.5 21.2
+#lcp,lpsa
+
+
 # They all select the same variables: 
 # lcp,lpsa
-
-
-
-
-
-
 
 
 ## 3 Make an appropriate LASSO model, with the appropriate link and error function, and
 ## evaluate the prediction performance. Do you see evidence that over-learning is an issue?
 
+set.seed(1)
+train=sample(1:nrow(omit_data), nrow(omit_data)*2/3)
+test=(-train)
+x=model.matrix(Cscore~.,omit_data)[,-1] #delete Cscore column
+y=omit_data$Cscore
+y.train=y[train]
+y.test=y[test]
+x.train=x[train,]
+x.test=x[test,]
+omit_data.train=omit_data[train,]
+omit_data.test=omit_data[test,]
+dim(omit_data.train) #64,8
+dim(omit_data.test) #32,8
 
+## Lasso regression
+par(mfrow=c(1,2))
+lasso.mod=glmnet(y=y.train,x=x.train,alpha=1)
+plot(lasso.mod)
+lasso.cv=cv.glmnet(x.train,y.train,alpha=1) # 10-fold cross validation
+plot(lasso.cv) 
 
+bestlam<-lasso.cv$lambda.min
+bestlam ## Select lamda that minimizes training MSE
+min(lasso.cv$cvm)
+# bestlam = 0.7 results in the smallest cross-validation error 643
+
+# Prediction and evaluation on test data
+lasso.pred=predict(lasso.mod,s=bestlam,newx=x.test)
+mean((lasso.pred-y.test)^2)
+# the test MSE = 750
+
+## linear regression
+linear.mod=lm(y.train~x.train)
+summary(linear.mod)
+# => lcavol, lcp, lpsa
+
+mean(linear.mod$residuals^2)
+# the training MSE = 460
+
+# use fitted model to make predictions
+linear.pred=predict(linear.mod,newx=x.test)
+mean((y.test-linear.pred)^2)
+# the test MSE = 2658
+
+# the MSE of the linear model (2658) is larger than the lasso model (761)
+# => the variance of lasso modle is smaller. 
+# => there is no over-learning issue.
 
 
 
 ## 4 Look at the coefficient for “lcavol” in your LASSO model. 
 ## Does this coefficient correspond to how well it can predict Cscore? Explain your observation.
 
+coef(lasso.cv)
+# lcavol, lweight, age, lbph coefficient = 0
+# svi = 6
+# lcp = 2
+# lpsa = 15 => important variables
 
 
 
